a4d9a99c6dcd64eb38b084e049c6e31d496263e5
Russell McClellan <russell.mcclellan@gmail.com>
2025-01-19 12:59:50 -0500

    add standard EQ shapes

--- rust/shared/dsp/src/iir/svf/mod.rs
+++ rust/shared/dsp/src/iir/svf/mod.rs
@@ -14,9 +14,22 @@ pub struct RawParams {
 }
 
 #[derive(Debug, Clone, Copy)]
+pub struct GainRawParams {
+    pub g: f64,
+    pub two_r: f64,
+    pub sqrt_gain: f64,
+}
+
+#[derive(Debug, Clone, Copy)]
 pub struct Input {
     pub x: f64,
     pub params: RawParams,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct GainInput {
+    pub x: f64,
+    pub params: GainRawParams,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -43,27 +56,28 @@ pub fn calc_two_r(q: f64) -> f64 {
 }
 
 impl Svf {
+    fn process_single(&mut self, input: Input) -> Output {
+        let Input { x, params } = input;
+        let RawParams { g, two_r: damping } = params;
+        // following https://www.native-instruments.com/fileadmin/ni_media/downloads/pdf/VAFilterDesign_2.1.0.pdf
+        let d = 1. / (1. + damping * g + g * g);
+        let high = d * (x - (damping + g) * self.s0 - self.s1);
+        let v0 = g * high;
+        let band = v0 + self.s0;
+        self.s0 = band + v0;
+        let v1 = g * band;
+        let low = v1 + self.s1;
+        self.s1 = low + v1;
+        Output { low, band, high }
+    }
+
     pub fn process<'a, I: IntoIterator<Item = Input> + 'a>(
         &'a mut self,
         inputs: I,
     ) -> impl Iterator<Item = Output> + '_ {
-        inputs.into_iter().map(
-            move |Input {
-                      x,
-                      params: RawParams { g, two_r: damping },
-                  }| {
-                // following https://www.native-instruments.com/fileadmin/ni_media/downloads/pdf/VAFilterDesign_2.1.0.pdf
-                let d = 1. / (1. + damping * g + g * g);
-                let high = d * (x - (damping + g) * self.s0 - self.s1);
-                let v0 = g * high;
-                let band = v0 + self.s0;
-                self.s0 = band + v0;
-                let v1 = g * band;
-                let low = v1 + self.s1;
-                self.s1 = low + v1;
-                Output { low, band, high }
-            },
-        )
+        inputs
+            .into_iter()
+            .map(move |input| self.process_single(input))
     }
 
     pub fn process_no_high<'a, I: IntoIterator<Item = Input> + 'a>(
@@ -123,6 +137,84 @@ impl Svf {
     ) -> impl Iterator<Item = f64> + '_ {
         self.process_no_high(inputs)
             .map(|OutputNoHigh { low, .. }| low)
+    }
+
+    pub fn process_high_shelf<'a, I: IntoIterator<Item = GainInput> + 'a>(
+        &'a mut self,
+        inputs: I,
+    ) -> impl Iterator<Item = f64> + '_ {
+        // For derivation, see https://www.dafx14.fau.de/papers/dafx14_aaron_wishnick_time_varying_filters_for_.pdf
+        inputs.into_iter().map(move |input| {
+            let GainInput {
+                x,
+                params:
+                    GainRawParams {
+                        g,
+                        two_r,
+                        sqrt_gain,
+                    },
+            } = input;
+            let Output { low, band, high } = self.process_single(Input {
+                x,
+                params: RawParams {
+                    g: g / sqrt_gain,
+                    two_r,
+                },
+            });
+            let gain = sqrt_gain * sqrt_gain;
+            gain * high + low + sqrt_gain * two_r * band
+        })
+    }
+
+    pub fn process_low_shelf<'a, I: IntoIterator<Item = GainInput> + 'a>(
+        &'a mut self,
+        inputs: I,
+    ) -> impl Iterator<Item = f64> + '_ {
+        // For derivation, see https://www.dafx14.fau.de/papers/dafx14_aaron_wishnick_time_varying_filters_for_.pdf
+        inputs.into_iter().map(move |input| {
+            let GainInput {
+                x,
+                params:
+                    GainRawParams {
+                        g,
+                        two_r,
+                        sqrt_gain,
+                    },
+            } = input;
+            let Output { low, band, high } = self.process_single(Input {
+                x,
+                params: RawParams {
+                    g: g * sqrt_gain,
+                    two_r,
+                },
+            });
+            let gain = sqrt_gain * sqrt_gain;
+            gain * low + high + sqrt_gain * two_r * band
+        })
+    }
+
+    pub fn process_bell<'a, I: IntoIterator<Item = GainInput> + 'a>(
+        &'a mut self,
+        inputs: I,
+    ) -> impl Iterator<Item = f64> + '_ {
+        // For derivation, see https://www.dafx14.fau.de/papers/dafx14_aaron_wishnick_time_varying_filters_for_.pdf
+        inputs.into_iter().map(move |input| {
+            let GainInput {
+                x,
+                params:
+                    GainRawParams {
+                        g,
+                        two_r,
+                        sqrt_gain,
+                    },
+            } = input;
+            let Output { low, band, high } = self.process_single(Input {
+                x,
+                params: RawParams { g, two_r },
+            });
+            let gain = sqrt_gain * sqrt_gain;
+            low + high + gain * two_r * band
+        })
     }
 
     pub fn reset(&mut self) {
