730456d97d664a3dda2bdcd6d09a16078f2f5e6c
Russell McClellan <russell.mcclellan@gmail.com>
2025-11-29 20:03:32 -0500

    elliptic downsampling filter (#37)

--- rust/jx_alpha/component/src/synth/voice/oscillators/downsampler.rs
+++ rust/jx_alpha/component/src/synth/voice/oscillators/downsampler.rs
@@ -4,19 +4,13 @@
 /// "Multirate Signal Processing for Communication Systems",
 /// Chapter 11. This is the nonlinear phase, 2-path version.
 ///
-/// We use a butterworth design. You can get the coefficients from a nonlinear solve,
-/// setting the filtur coefficients such that each numerator coefficient is equal to
-/// the coefficients of (z + 1)^n (numerator polynomial is palindromic so this works).
+/// We use an elliptic design for a short transition band.
 ///
-/// Or, you can use the explicit formula given in "Explicit Formulas for Lattice Wave Digital Filters" by
-/// Lajos Gazsi (note that lattice WDF is identical to the structure from harris's book).
-/// This formula:
+/// Honestly, as of this writing I have no idea whatsoever how to derive this from scratch,
+/// but "Explicit Formulas for Lattice Wave Digital Filters" by
+/// Lajos Gazsi 1985 has a design procedure for this.
 ///
-/// ```julia
-/// filter_order = 5
-/// N = 2 * filter_order + 1
-/// final_coeffs = [tan(pi * 2 / (4 * N) * i) ^ 2 for i in 1:filter_order]
-/// ```
+/// Note that lattice WDF is identical to the structure from harris's book.
 
 #[derive(Default, Debug, Clone)]
 pub struct Downsampler {
@@ -24,37 +18,28 @@ pub struct Downsampler {
     a1: f32,
     a2: f32,
     a3: f32,
-    a4: f32,
     b0: f32,
     b1: f32,
     b2: f32,
-    b3: f32,
-    b4: f32,
 }
 
 impl Downsampler {
     pub fn process(&mut self, input: [f32; 2]) -> f32 {
         let ia0 = input[1];
-        let ia1 = (ia0 - self.a1) * 0.008_586_551 + self.a0;
-        let ia2 = (ia1 - self.a2) * 0.080_954_21 + self.a1;
-        let ia3 = (ia2 - self.a3) * 0.247_945_07 + self.a2;
-        let ia4 = (ia3 - self.a4) * 0.570_274_1 + self.a3;
+        let ia1 = (ia0 - self.a1) * 0.061_861_712 + self.a0;
+        let ia2 = (ia1 - self.a2) * 0.433_903_75 + self.a1;
+        let ia3 = (ia2 - self.a3) * 0.878_088_06 + self.a2;
         self.a0 = ia0;
         self.a1 = ia1;
         self.a2 = ia2;
         self.a3 = ia3;
-        self.a4 = ia4;
         let ib0 = input[0];
-        let ib1 = (ib0 - self.b1) * 0.034_943_722 + self.b0;
-        let ib2 = (ib1 - self.b2) * 0.150_080_35 + self.b1;
-        let ib3 = (ib2 - self.b3) * 0.383_376_18 + self.b2;
-        let ib4 = (ib3 - self.b4) * 0.831_051_8 + self.b3;
+        let ib1 = (ib0 - self.b1) * 0.223_432_57 + self.b0;
+        let ib2 = (ib1 - self.b2) * 0.654_140_25 + self.b1;
         self.b0 = ib0;
         self.b1 = ib1;
         self.b2 = ib2;
-        self.b3 = ib3;
-        self.b4 = ib4;
-        f32::midpoint(ib4, ia4)
+        f32::midpoint(ib2, ia3)
     }
 
     pub fn reset(&mut self) {
