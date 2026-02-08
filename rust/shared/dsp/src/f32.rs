2440a3398fc28e178221282ca19f240b8dc27699
Russell McClellan <russell.mcclellan@gmail.com>
2026-02-08 16:00:28 -0500

    make sure helpers are inlined

--- rust/shared/dsp/src/f32.rs
+++ rust/shared/dsp/src/f32.rs
@@ -1,26 +1,33 @@
 use std::ops::RangeInclusive;
 
+#[inline]
 #[must_use]
 pub fn rescale_points(input: f32, from_low: f32, from_high: f32, to_low: f32, to_high: f32) -> f32 {
     let input = (input - from_low) / (from_high - from_low);
     input * (to_high - to_low) + to_low
 }
 
+#[inline]
 #[must_use]
 pub fn rescale(input: f32, from: RangeInclusive<f32>, to: RangeInclusive<f32>) -> f32 {
     rescale_points(input, *from.start(), *from.end(), *to.start(), *to.end())
 }
 
+#[inline]
 #[must_use]
 pub fn rescale_inverted(input: f32, from: RangeInclusive<f32>, to: RangeInclusive<f32>) -> f32 {
     rescale_points(input, *from.start(), *from.end(), *to.end(), *to.start())
 }
 
+#[inline]
 #[must_use]
 pub fn rescale_clamped(input: f32, from: RangeInclusive<f32>, to: RangeInclusive<f32>) -> f32 {
-    rescale(input, from.clone(), to.clone()).clamp(*to.start(), *to.end())
+    let to_low = *to.start();
+    let to_high = *to.end();
+    rescale_points(input, *from.start(), *from.end(), to_low, to_high).clamp(to_low, to_high)
 }
 
+#[inline]
 #[must_use]
 pub fn rescale_inverted_clamped(
     input: f32,
@@ -30,16 +37,19 @@ pub fn rescale_inverted_clamped(
     rescale_inverted(input, from.clone(), to.clone()).clamp(*to.start(), *to.end())
 }
 
+#[inline]
 #[must_use]
 pub fn lerp(value_at_zero: f32, value_at_one: f32, t: f32) -> f32 {
     value_at_zero * (1.0 - t) + value_at_one * t
 }
 
+#[inline]
 #[must_use]
 pub fn lerp_clamped(value_at_zero: f32, value_at_one: f32, t: f32) -> f32 {
     lerp(value_at_zero, value_at_one, t.clamp(0.0, 1.0))
 }
 
+#[inline]
 #[must_use]
 #[allow(clippy::cast_possible_truncation)]
 pub fn exp2_approx(x: f32) -> f32 {
