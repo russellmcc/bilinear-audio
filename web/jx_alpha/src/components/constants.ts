306f947e65125f82c8eb4afa2d87129fcb43265a
Russell McClellan <russell.mcclellan@gmail.com>
2026-04-11 18:34:35 -0400

    fix jank height rescaling by fixing all label heights

--- web/jx_alpha/src/components/constants.ts
+++ web/jx_alpha/src/components/constants.ts
@@ -1,6 +1,7 @@
 export const BALL_SIZE = 22;
 export const TRACK_LENGTH = 100;
 export const LABEL_MARGIN = 5;
+export const LABEL_HEIGHT = 12;
 export const DOT_SIZE = 4;
 export const DOT_OFFSET = 2;
 export const BORDER_WIDTH = 2;
